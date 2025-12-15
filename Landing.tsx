62353b25e2b0148e24b39b66816d1765c724db0e
BWalter <Walterchukwuemekaokougwu@outlook.com>
2025-12-15 22:53:13 +0100

    Update Landing.tsx

--- Landing.tsx
+++ Landing.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Landing.css";
-import logo from "../assets/ulohub.jpg";
+import logo from "./assets/ulohub.jpg";
 
 const Landing: React.FC = () => {
   const navigate = useNavigate();
@@ -31,3 +31,4 @@ const Landing: React.FC = () => {
 };
 
 export default Landing;
+
