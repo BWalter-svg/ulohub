8ebe97ec796823361f547450d71bc92c3cbf6688
BWalter <Walterchukwuemekaokougwu@outlook.com>
2025-12-17 10:52:49 +0100

    Update HelpPage.tsx

--- src/Pages/HelpPage.tsx
+++ src/Pages/HelpPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FiMail, FiPhone, FiMessageCircle, FiArrowLeft } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
-import supabase from "./../api/supabaseClient";
+import supabase from "../api/supabaseClient";
 import "./Landlord/landlord.css";
 
 const HelpPage: React.FC = () => {
@@ -139,5 +139,6 @@ const HelpPage: React.FC = () => {
     </div>
   );
 };
+
 
 export default HelpPage;
